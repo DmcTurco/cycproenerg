acaf1d14c548c04f38b139c8bcc03270f56f2f27
David Moises Mamani Chino <moisesanibal@hotmail.com>
2024-10-18 18:27:18 -0500

    tipo_documento no actualiza

--- app/Http/Controllers/Company/ClientController.php
+++ app/Http/Controllers/Company/ClientController.php
@@ -27,13 +27,10 @@ class ClientController extends Controller
     public function index(Request $request)
     {
 
-        // Obtenemos la lista de estados desde la base de datos
-        $estados = Estado::orderBy('nombre', 'DESC')->get(); // Esto obtiene todos los estados ordenados por nombre en orden ascendente
+        $estados = Estado::orderBy('nombre', 'DESC')->get();
 
-        // Comenzamos una consulta base de Solicitud con la relación 'estado'
         $query = Solicitud::with('estado');
 
-        // Aplicar filtros si están presentes en la solicitud
         if ($request->filled('numero_solicitud')) {
             $query->where('numero_solicitud', 'like', '%' . $request->numero_solicitud . '%');
         }
@@ -58,21 +55,16 @@ class ClientController extends Controller
             $query->where('numero_suministro', 'like', '%' . $request->numero_suministro . '%');
         }
 
-        // Ejecutar la consulta paginada
         $clientesConSolicitudes = $query->paginate(10)->appends($request->except('page'));
 
-        // Obtener el total de solicitudes
         $totalSolicitudes = Solicitud::count();
 
-        // Obtener el total de solicitudes filtradas
         $totalSolicitudesFiltradas = $query->count();
 
-        // Aplicar tipo_documento_nombre para cada solicitud como lo hacías antes
         foreach ($clientesConSolicitudes as $solicitud) {
             $solicitud->tipo_documento_nombre = $this->getTipoDocumentoName(optional($solicitud->solicitante)->tipo_documento);
         }
 
-        // Retornar la vista con los resultados
         return view('company.pages.clients.index', compact('clientesConSolicitudes', 'estados', 'totalSolicitudes', 'totalSolicitudesFiltradas'));
     }
 
@@ -97,126 +89,6 @@ class ClientController extends Controller
         }
         return date('Y-m-d', strtotime($date));
     }
-
-    // public function change(Request $request)
-    // {
-    //     // Validación del archivo cargado
-    //     $validator = Validator::make($request->all(), [
-    //         'file' => 'required|file|mimes:xlsx,xls'
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response()->json([
-    //             'success' => false,
-    //             'message' => 'El archivo debe ser un documento Excel válido (.xlsx o .xls).'
-    //         ], 422);
-    //     }
-
-    //     // Inicializar contadores y flag de error
-    //     $createdCount = 0;
-    //     $updatedCount = 0;
-    //     $hasErrors = false;
-
-    //     // Verificar si el archivo ha sido subido
-    //     if ($request->hasFile('file')) {
-    //         $file = $request->file('file');
-
-    //         try {
-    //             // Cargar el archivo Excel
-    //             $spreadsheet = IOFactory::load($file->getPathName());
-    //             $sheet = $spreadsheet->getActiveSheet();
-    //             $rows = $sheet->toArray(null, true, true, true);
-
-    //             // Verificar si el archivo está vacío
-    //             if (count($rows) <= 1) {
-    //                 throw new \Exception("El archivo Excel está vacío o solo contiene encabezados.");
-    //             }
-
-    //             // Iterar sobre las filas (omitir la primera fila si es el header)
-    //             foreach ($rows as $index => $row) {
-    //                 if ($index == 1) {
-    //                     continue; // Ignorar el header (primera fila)
-    //                 }
-
-    //                 // Validar que las columnas clave existen y tienen datos válidos
-    //                 if (empty($row['A']) || empty($row['H']) || empty($row['G']) || !is_numeric($row['H'])) {
-    //                     $hasErrors = true;
-    //                     continue;
-    //                 }
-
-    //                 // Procesamiento del solicitante (crear o actualizar)
-    //                 $solicitante = Solicitante::updateOrCreate(
-    //                     ['numero_documento' => trim($row['H'])],
-    //                     [
-    //                         'tipo_documento' => trim($row['G']),
-    //                         'nombre' => trim($row['I']),
-    //                         'celular' => trim($row['K']),
-    //                         'correo_electronico' => trim($row['L']),
-    //                         'usuario_fise' => trim($row['U']),
-    //                     ]
-    //                 );
-
-    //                 $empresa = Empresa::updatedOrCreate(
-    //                     ['numero_documento' => trim($row['AL'])],
-    //                     [
-    //                         'tipo_documento' => trim($row['AK']),
-    //                         'nombre' => trim($row['AM']),
-    //                         'registro_gas_natural' => trim($row['AN']),
-    //                     ]
-    //                 );
-
-    //                 $concesionaria = Concesionaria::updatedOrCreate(
-    //                     ['numero_documento' => trim($row['AP'])],
-    //                     [
-    //                         'tipo_documento' => trim($row['AO']),
-    //                         'nombre' => trim($row['AQ']),
-    //                     ]
-    //                 );
-
-    //                 $solicitud = Solicitud::updateOrCreate(
-    //                     ['solicitante_id' => $solicitante->id],
-    //                     ['empresa_id' => $empresa->id],
-    //                     ['concesionaria_id' => $concesionaria->id],
-    //                     [
-    //                         'numero_solicitud' => trim($row['A']) ?: null,
-    //                         'numero_suministro' => trim($row['C']) ?: null,
-    //                         'numero_contrato_suministro' => trim($row['D']) ?: null,
-    //                         'fecha_aprobacion_contrato' => $this->parseDate(trim($row['F'])),
-    //                         'fecha_registro_portal' => $this->parseDate(trim($row['X'])),
-    //                         'estado_solicitud' => trim($row['CO']),
-    //                     ]
-    //                 );
-
-    //                 $solicitante->wasRecentlyCreated ? $createdCount++ : $updatedCount++;
-    //             }
-
-    //             // Preparar el mensaje de respuesta
-    //             if ($hasErrors) {
-    //                 return response()->json([
-    //                     'success' => false,
-    //                     'message' => 'Se encontraron datos inconsistentes. Por favor, revise el archivo y vuelva a intentarlo.'
-    //                 ]);
-    //             } else {
-    //                 return response()->json([
-    //                     'success' => true,
-    //                     'message' => "Se agregaron $createdCount nuevos solicitantes y se actualizaron $updatedCount solicitantes existentes."
-    //                 ]);
-    //             }
-    //         } catch (\Exception $e) {
-    //             Log::error('Error procesando el archivo Excel: ' . $e->getMessage());
-
-    //             return response()->json([
-    //                 'success' => false,
-    //                 'message' => 'Error al procesar el archivo. Por favor, verifique el formato e intente nuevamente.'
-    //             ], 500);
-    //         }
-    //     }
-
-    //     return response()->json([
-    //         'success' => false,
-    //         'message' => 'No se ha subido ningún archivo.'
-    //     ], 400);
-    // }
 
     public function change(Request $request)
     {
@@ -329,9 +201,9 @@ class ClientController extends Controller
     {
         $tipo_documento_id = TipoDocumentoHelper::obtenerTipoDocumento(trim($row['G']));
         return Solicitante::updateOrCreate(
-            ['numero_documento' => trim($row['H'])],
+            ['numero_documento' => trim($row['H']),
+            'tipo_documento' => $tipo_documento_id,],
             [
-                'tipo_documento' => $tipo_documento_id,
                 'nombre' => trim($row['I']),
                 'celular' => trim($row['K']),
                 'correo_electronico' => trim($row['L']),
@@ -370,7 +242,6 @@ class ClientController extends Controller
             }
         }
 
-        // Si la abreviatura es muy corta (menos de 2 caracteres), usamos las dos primeras palabras
         if (strlen($iniciales) < 2) {
             return implode(' ', array_slice($palabras, 0, 2));
         }
@@ -378,23 +249,7 @@ class ClientController extends Controller
         return $iniciales;
     }
 
-    // private function obtenerAbreviatura($nombre)
-    // {
-    //     $palabrasClave = [
-    //         'registrada', 'observada', 'subsanada', 'aprobada', 'Habilitación registrado', 'Habilitación observada', 
-    //         'programada', 'iniciada', 'finalizada', 'sin descargo'
-    //     ];
 
-    //     foreach ($palabrasClave as $palabra) {
-    //         if (stripos($nombre, $palabra) !== false) {
-    //             return $palabra;
-    //         }
-    //     }
-
-    //     // Si no se encuentra una palabra clave, usamos las primeras palabras
-    //     $palabras = explode(' ', $nombre);
-    //     return implode(' ', array_slice($palabras, 0, 2));
-    // }
 
     private function processSolicitud($row, $solicitante, $empresa, $concesionaria, $estado)
     {
